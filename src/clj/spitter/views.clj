fa0972d43dc47f3db9b6dca92cc1826acedc07b0
Kristoffer Roupé <kitofr@gmail.com>
2013-11-06 21:59:14 +0100

    Added boostrap and updated view

--- src/clj/spitter/views.clj
+++ src/clj/spitter/views.clj
@@ -6,7 +6,7 @@
     [:head
      [:title "Spitter! (your daily speed reader)"]
      [:meta { :name "viewport" :content "width=device-width, initial-scale=1.0" }]
-     [:link { :href "http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" :rel "stylesheet" }]
+     [:link { :href "/js/components/bootstrap/dist/css/bootstrap.min.css" :rel "stylesheet" }]
      (include-css "/css/style.css")]
     [:body
      [:div.container
@@ -20,6 +20,6 @@
         [:button#reset.btn.btn-inverse "Reset"]]
        [:div#speed]]]
      [:script { :src "/js/spitter.js" }]
-     [:script { :src "http://code.jquery.com/jquery.js" }]
-     [:script { :src "http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js" }]]))
+     [:script { :src "/js/components/jquery/jquery.min.js" }]
+     [:script { :src "/js/components/bootstrap/dist/js/bootstrap.min.js" }]]))
                                       
